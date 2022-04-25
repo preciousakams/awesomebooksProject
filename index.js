aab1b5734f2f4146e7d1bc13408081902925dfe6
Felix Odete <felix.odette@gmail.com>
2022-04-25 19:57:54 +0300

    Minor lint fixes

--- index.js
+++ index.js
@@ -11,38 +11,37 @@ class UI {
     books.forEach((book) => UI.addBookToList(book));
   }
 
-  static addBookToList(book){
+  static addBookToList(book) {
     const list = document.querySelector('#book-Collection');
     const row = document.createElement('tr');
-    row.innerHTML =
-      `<td>${book.title}</td>
+    row.innerHTML = `<td>${book.title}</td>
       <td>${book.author}</td>
       <td><a href="#" class="btn delete">Remove</a></td>`;
     list.appendChild(row);
   }
 
-  static deleteBook(el){
-    if(el.classList.contains('delete')) {
+  static deleteBook(el) {
+    if (el.classList.contains('delete')) {
       el.parentElement.parentElement.remove();
     }
   }
 
-  static showAlert(message, className){
+  static showAlert(message, className) {
     const div = document.createElement('div');
-    div.className = `alert alert-${className}`
+    div.className = `alert alert-${className}`;
     div.appendChild(document.createTextNode(message));
     const container = document.querySelector('.container');
     const form = document.querySelector('#bookForm');
     container.insertBefore(div, form);
 
-    //Vanish in 3 seconds
+    //  Vanish in 3 seconds
     setTimeout(() => document.querySelector('.alert').remove(), 2000);
   }
 
   static clearFilds() {
-      document.querySelector('#title').value = '';
-      document.querySelector('#author').value = '';
-    }
+    document.querySelector('#title').value = '';
+    document.querySelector('#author').value = '';
+  }
 }
 
 // Storage
@@ -67,7 +66,7 @@ class BookList {
   deleteItem(book) {
     let books = BookList.getList();
     books.forEach((book, index) => {
-      if(book.title === book) {
+      if (book.title === book) {
         books.splice(index, 1);
       }
     });
