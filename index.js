4e80729bbd274d8d2c78a07c4e44287a9ab0c774
Precious Akams <preciousakams@yahoo.com>
2022-04-26 14:40:09 +0100

    class updated

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
-/* eslint-disable no-use-before-define */
 /* eslint-disable no-alert */
 /* eslint-disable max-classes-per-file */
 class Book {
   constructor(title, author) {
     this.title = title;
     this.author = author;
+  }
+}
+
+class BookList {
+  static getList() {
+    let books;
+    if (localStorage.getItem('books') === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem('books'));
+    }
+    return books;
+  }
+
+  static addItem(book) {
+    const books = BookList.getList();
+    books.push(book);
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+
+  static deleteItem(title) {
+    const books = BookList.getList();
+    books.forEach((book, index) => {
+      if (book.title === title) {
+        books.splice(index, 1);
+      }
+    });
+    localStorage.setItem('books', JSON.stringify(books));
   }
 }
 
@@ -33,36 +60,6 @@ class BookUI {
   static clearFilds() {
     document.querySelector('#title').value = '';
     document.querySelector('#author').value = '';
-  }
-}
-
-// Storage
-
-class BookList {
-  static getList() {
-    let books;
-    if (localStorage.getItem('books') === null) {
-      books = [];
-    } else {
-      books = JSON.parse(localStorage.getItem('books'));
-    }
-    return books;
-  }
-
-  static addItem(book) {
-    const books = BookList.getList();
-    books.push(book);
-    localStorage.setItem('books', JSON.stringify(books));
-  }
-
-  static deleteItem(title) {
-    const books = BookList.getList();
-    books.forEach((book, index) => {
-      if (book.title === title) {
-        books.splice(index, 1);
-      }
-    });
-    localStorage.setItem('books', JSON.stringify(books));
   }
 }
 
