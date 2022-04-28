1870afa0d565e8167cb20cad10343649c97882c0
Felix Odete <felix.odette@gmail.com>
2022-04-28 07:38:37 +0300

    Site with navigation

--- index.js
+++ index.js
@@ -1,4 +1,40 @@
 /* eslint-disable max-classes-per-file */
+
+const btnList = document.querySelector('#btn-list');
+const addNewBtn = document.querySelector('#btn-add-new');
+const contactBtn = document.querySelector('#btn-contact');
+
+const listSection = document.querySelector('.list');
+const addNewSection = document.querySelector('.addNew');
+const contactSection = document.querySelector('.contact');
+
+btnList.addEventListener('click', () => {
+  btnList.classList.add('active');
+  addNewBtn.classList.remove('active');
+  contactBtn.classList.remove('active');
+  listSection.style.display = 'block';
+  addNewSection.style.display = 'none';
+  contactSection.style.display = 'none';
+});
+
+addNewBtn.addEventListener('click', () => {
+  btnList.classList.remove('active');
+  addNewBtn.classList.add('active');
+  contactBtn.classList.remove('active');
+  listSection.style.display = 'none';
+  addNewSection.style.display = 'block';
+  contactSection.style.display = 'none';
+});
+
+contactBtn.addEventListener('click', () => {
+  btnList.classList.remove('active');
+  addNewBtn.classList.remove('active');
+  contactBtn.classList.add('active');
+  listSection.style.display = 'none';
+  addNewSection.style.display = 'none';
+  contactSection.style.display = 'flex';
+});
+
 class Book {
   constructor(title, author) {
     this.title = title;
